330252442b62ea93bfdf07d15f10b992adfbe97f
christian <christiancasey969@gmail.com>
2018-08-21 16:56:32 +0100

    social media site bootstrap done - include modal and scrollspy

--- projects/socialMedia-site/app.js
+++ projects/socialMedia-site/app.js
@@ -2,4 +2,25 @@
 console.log('JS loaded!');
 
 // Get the current year for the copyright
-$('#year').text(newDate().getFullYear());
+$('#year').text(new Date().getFullYear());
+
+// Init Scrollspy 
+
+$('body').scrollspy({ target: '#main-nav'});
+
+// Smooth Scrolling
+
+$('#main-nav a').on('click', function(event) {
+  if(this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+      
+      window.location.hash = hash;
+    });
+  }
+});
